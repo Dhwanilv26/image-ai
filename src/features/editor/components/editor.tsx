2a21e138e1a15674ac9c92fa9586784664503043
Dhwanilv26 <dhwanil2604@gmail.com>
2024-12-09 18:08:57 +0530

    creating editor layout

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -3,6 +3,10 @@
 import { useEditor } from '../hooks/use-editor';
 import { useEffect, useRef } from 'react';
 import { fabric } from 'fabric';
+import { Navbar } from './navbar';
+import { Sidebar } from './sidebar';
+import { Toolbar } from './toolbar';
+import { Footer } from './footer';
 export const Editor = () => {
   const { init } = useEditor();
 
@@ -21,9 +25,22 @@ export const Editor = () => {
   // sif init change hua to hi re render.. and usecallback mtlb no rendering bcz init is cached over there
   return (
     <div className="h-full flex flex-col ">
-      <div className="flex-1 h-[calc(100%-124px)] bg-muted " ref={containerRef}>
+      <Navbar/>
+      <div className='absolute h-[calc(100%-68px)] w-full top-[68px] flex'>
+        {/* using this div to prevent overflow and constant flickering issues */}
+
+        <Sidebar/>
+
+        <main className='bg-muted flex-1 overflow-auto relative flex flex-col'>
+          <Toolbar/>
+        <div className="flex-1 h-[calc(100%-124px)] bg-muted " ref={containerRef}>
         <canvas ref={canvasRef} />
       </div>
+
+      <Footer/>
+        </main>
+      
+    </div>
     </div>
   );
 };
---
+++ src/features/editor/components/toolbar.tsx
@@ -0,0 +1,7 @@
+export const Toolbar=()=>{
+
+    return (
+        <div className="shrink-0 h-[56px] border-b bg-white w-full flex-items-center overflow-x-auto z-[49] p-2 gap-x-2">toolbar</div>
+    )
+}
+// will contain specific options once selected according to the shape of the container
