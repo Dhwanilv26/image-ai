32eaaa43b8e8cfba30c40366caa59e44bc167b15
Dhwanilv26 <dhwanil2604@gmail.com>
2024-12-10 12:16:01 +0530

    implemented the complete shape-sidebar frontend

--- src/features/editor/components/shape-sidebar.tsx
+++ src/features/editor/components/shape-sidebar.tsx
@@ -1,6 +1,12 @@
 import { ActiveTool } from "../types";
 import { cn } from "@/lib/utils";
 import { ToolSidebarHeader } from "./tool-sidebar-header";
+import { ToolSidebarClose } from "./tool-sidebar-close";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ShapeTool } from "./shape-tool";
+import { FaCircle, FaSquare, FaSquareFull } from "react-icons/fa";
+import { IoTriangle } from "react-icons/io5";
+import { FaDiamond } from "react-icons/fa6";
 interface ShapeSidebarProps{
     activeTool:ActiveTool;
     onChangeActiveTool:(tool:ActiveTool)=>void;
@@ -8,6 +14,10 @@ interface ShapeSidebarProps{
 
 export const ShapeSidebar=({activeTool,onChangeActiveTool}:ShapeSidebarProps)=>{
 // yaha par class hi aisa banaya hai dynamic ki click hone par hi dikhega.. bcz click hone par hi activetool shapes hoga so
+
+    const onClose=()=>{
+        onChangeActiveTool('select');
+    }
     return (
        <aside
     className={cn('bg-white relative border-r z-[40] w-[360px] h-full flex flex-col ',
@@ -17,6 +27,32 @@ export const ShapeSidebar=({activeTool,onChangeActiveTool}:ShapeSidebarProps)=>{
         <ToolSidebarHeader
         title="Shapes"
         description="Add shapes to your canvas"/>
+
+        <ScrollArea>
+            <div className="grid grid-cols-3 gap-4 p-4">
+                <ShapeTool 
+                onClick={()=>{}}
+                icon={FaCircle}/>
+                 <ShapeTool 
+                onClick={()=>{}}
+                icon={FaSquare}/>
+                 <ShapeTool 
+                onClick={()=>{}}
+                icon={FaSquareFull}/>
+                 <ShapeTool 
+                onClick={()=>{}}
+                icon={IoTriangle}/>
+                 <ShapeTool 
+                onClick={()=>{}}
+                icon={IoTriangle}
+                iconClassName="rotate-180"/>
+                 <ShapeTool 
+                onClick={()=>{}}
+                icon={FaDiamond}/>
+            </div>
+            </ScrollArea>
+
+        <ToolSidebarClose onClick={onClose}/>
     </aside>
     )
 }
