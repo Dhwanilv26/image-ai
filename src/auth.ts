46068aaa701679a00b96b3deefb1461375e6cb30
Dhwanilv26 <dhwanil2604@gmail.com>
2024-12-16 12:37:42 +0530

    implemented the initial neon and drizzle setup and migrated user data for auth js

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,11 @@
 import NextAuth from 'next-auth';
-import GitHub from "next-auth/providers/github"
+import GitHub from 'next-auth/providers/github';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+
+import { db } from './db/drizzle';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub], // reads the github id and github secret from the .env file
+  adapter: DrizzleAdapter(db),
+  providers: [GitHub], 
 });
+ 
